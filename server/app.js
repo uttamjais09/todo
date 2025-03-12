e34881ad38be99c797cc9bea45120a04b883a40a
uttam jaiswal <uttamjaiswal09soft@gmail.com>
2025-03-12 20:04:19 +0530

    add all

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ import { dirname } from 'path/posix';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000 ;
 const __dirname = path.resolve();
 
 

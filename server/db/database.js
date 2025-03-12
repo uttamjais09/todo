11efd70a36aeb7adf7a9559969654a6a4faacfb4
uttam jaiswal <uttamjaiswal09soft@gmail.com>
2025-03-12 23:01:05 +0530

    all

--- server/db/database.js
+++ server/db/database.js
@@ -6,8 +6,7 @@ const connectDB = async () =>{
     
 try{
     await mongoose.connect(process.env.MONGODB_URI , {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+       
         serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
         socketTimeoutMS: 45000
     });
